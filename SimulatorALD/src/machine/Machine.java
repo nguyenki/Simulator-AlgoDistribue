a3026c39024d44db21186a5fa03ea4103b4084f6
nguyenki <kim-thuat.nguyen@ensimag.imag.fr>
2012-11-20 10:22:44 +0100

    init depot et resources

---
+++ SimulatorALD/src/machine/Machine.java
@@ -0,0 +1,33 @@
+package machine;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Kim Thuat Nguyen
+ */
+public class Machine {
+    private TypeMessage typeMessage;
+    private int capacCarte;
+
+    public TypeMessage getTypeMessage() {
+        return typeMessage;
+    }
+
+    public int getCapacCarte() {
+        return capacCarte;
+    }
+
+    public void setTypeMessage(TypeMessage typeMessage) {
+        this.typeMessage = typeMessage;
+    }
+
+    public void setCapacCarte(int capacCarte) {
+        this.capacCarte = capacCarte;
+    }
+    
+    
+}
---
+++ SimulatorALD/src/message/Message.java
@@ -0,0 +1,24 @@
+package message;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Kim Thuat Nguyen
+ */
+public class Message {
+    private int taille;
+
+    public int getTaille() {
+        return taille;
+    }
+
+    public void setTaille(int taille) {
+        this.taille = taille;
+    }
+    
+    
+}
